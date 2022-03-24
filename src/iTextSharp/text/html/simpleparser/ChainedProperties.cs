bf901f1d24c1c37c9913ceeb86299b9c4dec2f19
Moacyr Rodrigues Pereira <moacyr@m3sistemas.com.br>
2022-03-24 10:39:01 -0300

    project moved to src folder

--- src/iTextSharp/text/html/simpleparser/ChainedProperties.cs
+++ src/iTextSharp/text/html/simpleparser/ChainedProperties.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Windows.Forms;
 
 /*
  * Copyright 2004 Paulo Soares
